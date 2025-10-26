b607dc98159e68b7898212569498fd5249106c5b
ofirswheelie@gmail.com <ofirswheelie@gmail.com>
2025-10-27 00:26:31 +0200

    Fix version: Remove SNAPSHOT suffix
    - Changed version from 1.0-SNAPSHOT to 1.0.0
    - Updated untilBuild from 242.* to 252.* for consistency
    - Ready for JetBrains Marketplace submission

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.example"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -29,7 +29,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("232")
-        untilBuild.set("242.*")
+        untilBuild.set("252.*")
     }
 
     signPlugin {
